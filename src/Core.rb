90a4b976b25bd8354a2bd4cd351a71ab2caa8c41
Belén Albeza <belen.albeza@gmail.com>
2013-12-14 16:37:02 +0000

    ADD simple one point collision

--- src/Core.rb
+++ src/Core.rb
@@ -1,8 +1,14 @@
 class Game < Gosu::Window
+  attr_reader :delta, :images
+
   def initialize
-    super 800, 600, true
+    super 800, 600, false
     self.caption = 'LD28 by ladybenko'
-    @current_scene = Scene.new(self)
+
+    @current_scene = Scene.new
+    @old_timestamp = 0
+
+    load_images
   end
 
   def button_up(key)
@@ -14,22 +20,37 @@ class Game < Gosu::Window
   end
 
   def update
+    self.update_delta
     @current_scene.update
   end
 
   def switch_scene(scene_class)
-    @current_scene = scene_class.new(self)
+    @current_scene = scene_class.new
+  end
+
+  protected
+
+  def load_images
+    @images = {
+      :grass => Gosu::Image.new(self, 'gfx/tiles/grass.png', true),
+      :hero => Gosu::Image.new(self, 'gfx/chara.png')
+    }
+  end
+
+  def update_delta
+    now = Gosu::milliseconds
+    @delta = [(now - @old_timestamp) / 1000.0, 0.25].min
+    @old_timestamp = now
   end
 end
 
 class Scene
-  def initialize(window)
-    @window = window
-  end
-
   def update
   end
 
   def draw
+  end
+
+  def button_up(key)
   end
 end
--- src/World.rb
+++ src/World.rb
@@ -1,28 +1,35 @@
+class Numeric
+  def sign
+    self >= 0 ? 1 : -1
+  end
+end
+
 class PlayScene < Scene
-  def initialize(window)
-    super window
-    @images = {
-      :grass => Gosu::Image.new(@window, 'gfx/tiles/grass.png', true),
-      :hero => Gosu::Image.new(@window, 'gfx/chara.png')
-    }
-    @map = Map.new(@images)
+  def initialize
+    super
+    @map = Map.new
+    @hero = Hero.new(@map)
   end
 
   def button_up(key)
-    @window.close if key == Gosu::KbEscape
+    $game.close if key == Gosu::KbEscape
   end
 
   def draw
-    @images[:hero].draw(48, 48, 0)
     @map.draw
+    @hero.draw
+  end
+
+  def update
+    @hero.update
   end
 end
 
 class Map
   SIZE = 48
 
-  def initialize(images)
-    @images = images
+  def initialize
+    @images = $game.images
     @tiles = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -44,17 +51,32 @@ class Map
   def draw
     self.rows.times do |row|
       self.cols.times do |col|
-        img = self.tile(col, row)
+        img = self.gfx_tile(col, row)
         img.draw(col * SIZE, row * SIZE, 0) unless img.nil?
       end
     end
   end
 
-  def tile(x, y)
-    case @tiles[y][x]
-    when 0 then nil
+  def gfx_tile(col, row)
+    case @tiles[row][col]
     when 1 then @images[:grass]
+    else nil
     end
+  end
+
+  def phy_tile(col, row)
+    if col < self.cols and col >= 0 and row < self.rows and row >= 0
+      case @tiles[row][col]
+      when 1 then :solid
+      else :void
+      end
+    else
+      nil
+    end
+  end
+
+  def screen_to_map(x, y)
+    [x / SIZE, y / SIZE]
   end
 
   def rows
@@ -63,5 +85,83 @@ class Map
 
   def cols
     @tiles.first.size
+  end
+end
+
+module Entity
+  @x = 0
+  @y = 0
+  @map = nil
+  @width = Map::SIZE
+  @height = Map::SIZE
+
+  def clamp_inc(inc_x, inc_y)
+    # clamp x
+    clamped_inc_x = 0
+    if inc_x != 0
+      0.upto(inc_x.abs) do |i|
+        if self.phy_tile(@x + i * inc_x.sign, @y) == :solid
+          break
+        else
+          clamped_inc_x = i * inc_x.sign
+        end
+      end
+    end
+
+    # clamp y
+    clamped_inc_y = 0
+    if inc_y != 0
+      0.upto(inc_y.abs) do |i|
+        if self.phy_tile(@x, @y + i * inc_y.sign) == :solid
+          break
+        else
+          clamped_inc_y = i * inc_y.sign
+        end
+      end
+    end
+
+    [clamped_inc_x, clamped_inc_y]
+  end
+
+  def phy_tile(x, y)
+    col, row = @map.screen_to_map(x, y)
+    @map.phy_tile(col, row)
+  end
+end
+
+class Hero
+  include Entity
+
+  def initialize(map)
+    @image = $game.images[:hero]
+    @map = map
+    @x = 0
+    @y = 0
+  end
+
+  def draw
+    @image.draw_rot @x, @y, 1, 0
+  end
+
+  def update
+    if $game.button_down?(Gosu::KbRight)
+      inc_x = 300 * $game.delta
+    elsif $game.button_down?(Gosu::KbLeft)
+      inc_x = -300 * $game.delta
+    else
+      inc_x = 0
+    end
+
+    if $game.button_down?(Gosu::KbDown)
+      inc_y = 300 * $game.delta
+    elsif $game.button_down?(Gosu::KbUp)
+      inc_y = -300 * $game.delta
+    else
+      inc_y = 0
+    end
+
+    inc_x, inc_y = self.clamp_inc(inc_x, inc_y)
+    @x += inc_x
+    @y += inc_y
   end
 end
